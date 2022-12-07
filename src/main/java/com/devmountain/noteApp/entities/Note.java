967b5dbcc5f649cf10e96ccce9bf29be6d2644e2
asya-code <77514066+asya-code@users.noreply.github.com>
2022-12-06 17:47:19 -0800

    copy of the noteApp due to unresolved bug with maven's backlog

---
+++ src/main/java/com/devmountain/noteApp/entities/Note.java
@@ -0,0 +1,45 @@
+package com.devmountain.noteApp.entities;
+import javax.persistence.*;
+
+import com.fasterxml.jackson.annotation.JsonBackReference;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import com.devmountain.noteApp.entities.User;
+
+
+@Entity
+@Table(name="Notes")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Note {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(columnDefinition = "text")
+    private String body;
+
+    @ManyToOne
+    @JsonBackReference
+    private User user;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getBody() {
+        return body;
+    }
+
+    public void setBody(String body) {
+        this.body = body;
+    }
+
+}
